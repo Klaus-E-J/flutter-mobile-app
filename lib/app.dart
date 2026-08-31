104075f687ea7cb0a1fc12bd6a6f533ee4ad103a
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2026-08-31 20:16:39 -0300

    feat: Tela de Questoes - Teste

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
+import 'screens/provas/prova_questoes_screen.dart'; // TODO: remover após teste
 
 class AvaliaProApp extends StatelessWidget {
   const AvaliaProApp({super.key});
@@ -12,8 +13,10 @@ class AvaliaProApp extends StatelessWidget {
       title: 'Avalia Pro',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
-      initialRoute: AppRoutes.access,
-      onGenerateRoute: AppRoutes.onGenerateRoute,
+      // TODO: reverter após teste
+      // initialRoute: AppRoutes.access,
+      // onGenerateRoute: AppRoutes.onGenerateRoute,
+      home: const ProvaQuestoesScreen(provaTitulo: 'Prova de Teste'),
     );
   }
 }
