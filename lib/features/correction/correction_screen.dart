04c27b1f2d636f9c625e119f5e795287adc26d53
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2026-09-02 21:56:32 -0300

    feat: Adicionando fluxo da tela de correção no botão
    Feat/dashboard

--- lib/features/correction/correction_screen.dart
+++ lib/features/correction/correction_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../screens/correcao/selecao_prova_screen.dart';
+
 class CorrectionScreen extends StatelessWidget {
   const CorrectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Correção')));
+    return const SelecaoProvaScreen();
   }
 }
