485c27e7da9d459b40767d603b4de9ae5173dc2b
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2026-09-02 21:56:32 -0300

    feat: Adicionando fluxo da tela de correção no botão
    Feat/dashboard

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
-import 'screens/correcao/selecao_prova_screen.dart'; // TODO: remover após teste
 
 class AvaliaProApp extends StatelessWidget {
   const AvaliaProApp({super.key});
@@ -13,10 +12,8 @@ class AvaliaProApp extends StatelessWidget {
       title: 'Avalia Pro',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
-      // TODO: reverter após teste
-      // initialRoute: AppRoutes.access,
-      // onGenerateRoute: AppRoutes.onGenerateRoute,
-      home: const SelecaoProvaScreen(),
+      initialRoute: AppRoutes.access,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
     );
   }
 }
